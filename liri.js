ef657f084a897ed61ef589963d4b4f7ab62416ce
Clark <clarkphan@github.com>
2017-12-19 23:04:25 -0800

    add song search functionality

--- liri.js
+++ liri.js
@@ -3,11 +3,17 @@ var twitterKeys = require('./keys.js')
 
 if (process.argv[2]) {
   var command = process.argv[2].toLowerCase()
+  if (process.argv[3]) {
+    var song = process.argv[3].toLowerCase()
+  }
 }
 
 switch (command) {
   case 'my-tweets':
     displayTweets()
+    break
+  case 'spotify-this-song':
+    searchSong(song)
     break
 }
 
@@ -27,13 +33,29 @@ function displayTweets () {
     if (!error) {
       if (tweets.length >= 20) {
         for (var i = 0; i < 20; i++) {
-          console.log(tweets[i].created_at + " " + tweets[i].text)
+          console.log(tweets[i].created_at + ' ' + tweets[i].text)
         }
       } else {
         for (var i = 0; i < tweets.length; i++) {
-          console.log(tweets[i].created_at + " " + tweets[i].text)
+          console.log(tweets[i].created_at + ' ' + tweets[i].text)
         }
       }
     }
+  })
+}
+
+function searchSong (song) {
+  var Spotify = require('node-spotify-api')
+
+  var spotify = new Spotify({
+    id: 'b694be677e3c458b9b1ca394fc95404b',
+    secret: 'c80f87cdce354508931da4dc8d50899f'
+  })
+
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err)
+    }
+    console.log(data.tracks.items[0].artists[0].name)
   })
 }
