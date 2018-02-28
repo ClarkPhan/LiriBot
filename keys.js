f7ce771917581ca356943970bbedcd35a57abfdf
Clark Phan <clarkphan24@gmail.com>
2018-02-28 14:28:54 -0800

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-var twitterKeys = {
-  consumer_key: 'Cp4KYVKYjYNre24qGCBYqLNoh',
-  consumer_secret: 'AK3nfIlgV1F2JOF8kYkiyfHhaUZ3LXiUFJ32p0d0Qv0kAyDFqU',
-  access_token_key: '943356841232420864-SKsUSAuGM8nOZm7XOqX89vMt3bMsDjI',
-  access_token_secret: 'jVACmHho4diRaB5LsycyiYdHVuXjsbpkkmRbxztgNbyfK',
-}
-
-module.exports = twitterKeys;
